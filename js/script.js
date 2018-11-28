7890e8fc907b252c21e9ee7c8aa91e0fb3c09a6b
RachaelHead <rachaelh@live.unc.edu>
2018-11-28 13:35:22 -0500

    added polygon on map, buttons for art categories

--- js/script.js
+++ js/script.js
@@ -5,13 +5,18 @@
     lng: -72.5471516
   };
 
+  var machuPicchuPano = {
+    lat: -13.1649599,
+    lng: -72.5448498
+  };
+
 
   function initMap() {
 
     // The map, centered at Machu Picchu
     var map = new google.maps.Map(
         document.getElementById('map'), {
-          zoom: 4,
+          zoom: 3.5,
           center: machuPicchu
     });//close map
 
@@ -23,15 +28,127 @@
       icon: ''
     });//close marker
 
+    //shape
+
+    // Define the LatLng coordinates for the polygon's path.
+    var empireCoords = [
+      {lat: 1.559829, lng: -78.989244},
+      {lat: 0.981642, lng:  -76.324366},
+      {lat: -2.5456491, lng: -75.7562099},
+      {lat: -2.171240, lng: -77.662903},
+      {lat: -5.063275,lng:  -78.130839},
+      {lat: -5.656196, lng:  -77.670287},
+      {lat: -5.739603,  lng: -76.719467},
+      {lat: -6.479062,  lng: -76.045514},
+      {lat: -7.363231, lng:  -76.205597},
+      {lat: -8.852017, lng:  -75.852173},
+      {lat: -9.564806, lng: -74.881637},
+      {lat: -10.507772, lng:  -74.749240},
+      {lat: -11.077693,  lng:  -74.490429},
+      {lat: -11.016860, lng: -73.657276},
+      {lat: -11.203184, lng:   -73.210634},
+      {lat: -11.744455, lng:  -72.870818},
+      {lat: -11.380662, lng:   -72.176674},
+      {lat: -11.644480,  lng:  -71.613347},
+      {lat: -12.539197,  lng:  -71.289643},
+      {lat: -14.765036, lng:  -67.708335},
+      {lat: -16.158788, lng:   -65.329268},
+      {lat: -17.715863, lng:   -64.904487},
+      {lat: -22.025556,  lng:   -65.329820},
+      {lat: -23.907807, lng: -65.752557},
+      {lat: -25.364617,  lng: -65.231245},
+      {lat: -27.627925, lng:  -66.362081},
+      {lat: -29.298748, lng:    -68.759568},
+      {lat: -30.430610, lng:  -68.814722},
+      {lat: -32.610200, lng:  -70.177381},
+      {lat:-34.181236, lng: -69.847118},
+      {lat: -35.802139, lng: -72.501315 },
+      {lat: -34.151133,  lng: -71.991064},
+      {lat: -33.617284, lng: -71.592321},
+      {lat: -33.110427, lng:  -71.699636},
+      {lat: -32.385563,  lng: -71.426263},
+      {lat: -31.160423, lng:  -71.681949},
+      {lat: -30.249921, lng:  -71.643856},
+      {lat: -30.177113, lng:  -71.461229},
+      {lat: -30.009914, lng:  -71.380548},
+      {lat:-29.596270, lng:  -71.275413},
+      {lat: -29.232693, lng:  -71.461104 },
+      {lat: -28.876730, lng: -71.499905},
+      {lat: -27.639039,  lng: -70.895136},
+      {lat: -27.164049,lng:  -70.931919},
+      {lat: -26.022304, lng:  -70.639156},
+      {lat: -25.784559, lng: -70.726984},
+      {lat: -25.558136, lng:  -70.651512},
+      {lat:-25.422126, lng:  -70.451039},
+      {lat: -23.865160, lng:  -70.514462},
+      {lat: -22.987760, lng:  -70.320419},
+      {lat: -20.571127,  lng:  -70.172649},
+      {lat: -18.356273, lng: -70.371614},
+      {lat: -17.663890,  lng:  -71.312766},
+      {lat: -17.332920, lng:  -71.408975},
+      {lat: -15.371948, lng: -75.162022},
+      {lat: -13.883049,  lng:  -76.390015},
+      {lat: -12.486317,  lng:  -76.791189},
+      {lat: -12.414763,  lng: -76.778425},
+      {lat: -12.273262,  lng: -76.883338},
+      {lat: -12.195589, lng:  -77.044757},
+      {lat: -12.134915,  lng: -77.034807},
+      {lat: -12.069303,  lng:  -77.163860},
+      {lat: -11.589693,  lng: -77.276322 },
+      {lat: -11.467235,  lng:  -77.350474},
+      {lat: -11.297581,  lng:  -77.648400},
+      {lat: -11.219494,  lng: -77.644242 },
+      {lat: -11.173840,  lng:  -77.594923},
+      {lat: -10.843453,  lng:  -77.722627},
+      {lat: -10.107024,  lng:  -78.184270},
+      {lat: -10.084703,  lng:  -78.167056},
+      {lat: -9.856168,  lng: -78.242037},
+      {lat: -9.798699,  lng: -78.227441},
+      {lat: -8.456333,  lng:  -78.896999},
+      {lat: -6.759783,  lng:  -79.986102},
+      {lat: -5.998294,  lng: -81.137287},
+      {lat: -4.661166, lng:  -81.306308},
+      {lat: -4.231621,  lng:  -81.229405},
+      {lat: -2.640135,  lng:  -79.759498 },
+      {lat: -2.715860,   lng: -80.266939},
+      {lat: -2.2278276,   lng: -80.9659767},
+      {lat: -1.969065,  lng:  -80.757149},
+      {lat: -1.602420,  lng:  -80.849295 },
+      {lat: -1.476388,  lng:-80.789997},
+      {lat: -1.357109,  lng:  -80.734708},
+      {lat: -1.054543,   lng:  -80.909310},
+      {lat: -0.957039,   lng:  -80.832193},
+      {lat: -0.899739,  lng:  -80.555797},
+      {lat: -0.373173,   lng:  -80.483736},
+      {lat: 0.054778,  lng:  -80.067693 },
+      {lat: 0.679315,   lng:   -80.099300},
+      {lat: 0.822230,   lng:  -80.052359},
+      {lat: 0.991416,  lng:  -79.654633},
+      {lat: 1.371370,    lng:  -78.904452},
+      {lat: 1.430158,    lng:  -78.809703},
+      {lat: 1.645750,    lng: -79.005708}
+    ];
+
+    // Construct the polygon.
+    var incaEmpire = new google.maps.Polygon({
+      paths: empireCoords,
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35
+    });
+    incaEmpire.setMap(map);
+
 
     //street view
 
     var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('pano'), {
-          position: machuPicchu,
+          position: machuPicchuPano,
           pov: {
-            heading: 160,
-            pitch: 10
+            heading: 3.11,
+            pitch: 5
           }
         });//close panorama
     map.setStreetView(panorama);
@@ -68,6 +185,10 @@ $(function(){
   var object21= 'https://collectionapi.metmuseum.org/public/collection/v1/objects/316926';
   var metObjectArray = [object1, object2, object3, object4, object5, object6, object7, object8, object9, object10, object11, object12, object13, object14, object15, object16, object17, object18, object19, object20, object21];
 
+  $('#textiles-button').click(function(){
+      console.log('button clicked');
+
+
   var data = [];
   var html = '';
 
@@ -95,5 +216,7 @@ $(function(){
     });// close ajax
 
   }//close for loop
+
+});// close click function
 
 });//close wrapper
